e7a55d7dcd009aa3a993377c3b491a5eaadf5481
IaroslavLasiichuk <lasmant@yahoo.com>
2023-01-10 16:38:21 -0700

    Created comment for variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+ // Set up the character lists that can be included in the password
+
+// Initialize the password as an empty array
+var password = [];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button

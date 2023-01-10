a1d5f181399d631ccecf1b8c97e6b1983a4706b9
IaroslavLasiichuk <lasmant@yahoo.com>
2023-01-10 16:42:42 -0700

    Added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
- // Set up the character lists that can be included in the password
+// Set up the character lists that can be included in the password
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
+var upperCases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Initialize the password as an empty array
 var password = [];

3b62b4ddcadb6aab7e36d81ccdacc647bedb6a99
IaroslavLasiichuk <lasmant@yahoo.com>
2023-01-10 20:21:46 -0700

    Code optimization

--- Develop/script.js
+++ Develop/script.js
@@ -2,10 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // Set up the character lists that can be included in the password
+var lowerCases = "abcdefghijklmnopqrstuvwxyz";
+var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialCharacters = "!@#$%^&*()_+-=[]{};':\"|.<>/?\\";
-var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCases = "abcdefghijklmnopqrstuvwxyz";
 
 // Initialize the password as an empty array
 var password = [];
@@ -13,9 +13,10 @@ var password = [];
 function generatePassword() {
   // Promt user to start password generator
   var passwordLength = prompt("Enter the desired password length (8-128 characters):");
+
    // Validate the user's input
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("Enter correct number");
+    alert("Please enter correct number at least 8 characters and no more than 128 characters");
     return;
   }
   // Promt user to select for character types to include in the password
@@ -23,31 +24,29 @@ function generatePassword() {
   var includeUppercase = confirm("Include uppercase characters in the password?");
   var includeNumeric = confirm("Include numeric characters in the password?");
   var includeSpecial = confirm("Include special characters in the password?");
+
    // Use a for loop to continuously prompt the user for password criteria until at least one character type is selected
   if (passwordLength > 8 && passwordLength < 128) {
     for (let i = 0; i < passwordLength; i++) {
+
          // Initialize the character list as an empty string
           var charList = "";
 
           // Prompt the user for password criteria
           if (includeLowercase) {
               charList += lowerCases;
-              console.log(charList);
           }
 
           if (includeUppercase) {
               charList += upperCases;
-              console.log(charList);
           }
 
           if (includeNumeric) {
               charList += numbers;
-              console.log(charList);
           }
 
           if (includeSpecial) {
               charList += specialCharacters;
-              console.log(charList);
           }
 
           // Generate a random index to select a character from the charList
@@ -55,7 +54,6 @@ function generatePassword() {
 
           // Add the character at the randomly-selected index to the password
           password += charList[index];
-    
       }
   }
       // Validate the user's input
@@ -63,8 +61,7 @@ function generatePassword() {
       alert("You must select at least one character type to include in the password!");
       generatePassword();
   }
-  return password
-
+  return password;
 }
 
 // Write password to the #password input
